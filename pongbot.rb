6a9c5035d5ad38f7519095e363dd89a6cb01f0ee
Azzapop <aaron.b.hook@gmail.com>
2018-05-03 15:38:55 +1000

    ACTUALLY enable logging

--- pongbot.rb
+++ pongbot.rb
@@ -1,11 +1,13 @@
 class Pongbot < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
 
   set :public_folder => "public", :static => true
 
   get "/" do
     puts params
     user = User.create(name: rand(7), slack_id: rand(7))
-    puts user.inspect
     erb :welcome
   end
 

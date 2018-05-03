5ffc122421a8b6a385a8a56aeb753cfc92158057
Azzapop <aaron.b.hook@gmail.com>
2018-05-03 20:49:07 +1000

    Add correct response

--- pongbot.rb
+++ pongbot.rb
@@ -13,9 +13,13 @@ class Pongbot < Sinatra::Base
     # slack params
     # {"token"=>"", "team_id"=>"", "team_domain"=>"", "channel_id"=>"", "channel_name"=>"general", "user_id"=>"U5PAL827N", "user_name"=>"aaron", "command"=>"/pongbot", "text"=>"test test", "response_url"=>"https://hooks.slack.com/commands/T5PBY04J1/358443814534/EeABz3I4xMp5YQ5NPYTU2D6X", "trigger_id"=>"357551349650.193406004613.4aa7b53bdeee78a106cd03828a8eaa78"}
     # user type <@U5PAL827N|aaron>
+
     query = params['text'].split(' ')
     if query[0] != 'record'
-      return # fuck that shit
+      return {
+        "response_type": "in_channel",
+        "text": "Sorry, that didn't work. Please try again."
+      }.to_json
     end
     keys = [:slack_id, :name]
     winner_params = Hash[keys.zip(query[1].tr('<>', '').split('|'))]
@@ -35,10 +39,15 @@ class Pongbot < Sinatra::Base
       loser = Loser.create(loser_params)
     end
 
-
-    match = Match.record(winner: winner, loser: loser)
+    _match = Match.record(winner: winner, loser: loser)
 
     logger.info 'thats all there is, there isnt anymore'
+
+    status 200
+    return {
+      "response_type": "in_channel",
+      "text": "Sorry, that didn't work. Please try again."
+    }.to_json
 
     # validate token
     # find or create winner

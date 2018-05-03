d56cf2e1c5a0ff8627d1f65a3182ca1ae9554e03
Azzapop <aaron.b.hook@gmail.com>
2018-05-03 14:59:17 +1000

    add record method

--- pongbot.rb
+++ pongbot.rb
@@ -3,6 +3,13 @@ class Pongbot < Sinatra::Base
   set :public_folder => "public", :static => true
 
   get "/" do
+    puts params
+    user = User.create(name: rand(7), slack_id: rand(7))
+    puts user.inspect
     erb :welcome
+  end
+
+  post "/record" do
+    puts params
   end
 end

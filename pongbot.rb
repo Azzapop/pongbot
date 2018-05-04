5a692ec6e5477e583722baba5dfdcd7642002261
Azzapop <aaron.b.hook@gmail.com>
2018-05-04 12:16:10 +1000

    try delete_all

--- pongbot.rb
+++ pongbot.rb
@@ -24,7 +24,7 @@ class Pongbot < Sinatra::Base
       attachments: []
     }
 
-    User.delete
+    User.delete_all
     logger.info User.count
     if params['token'] != ENV['SLACK_TOKEN']
       status 403
